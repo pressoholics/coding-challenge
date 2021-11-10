ac7a36786dd07de24aa029c1e0e26e492849a03a
BENJAMIN MOODY <email@benjaminmoody.com>
2021-11-10 08:53:53 -0500

    initial code layout cleanup

--- php/Block.php
+++ php/Block.php
@@ -47,80 +47,81 @@ class Block {
 	 */
 	public function register_block() {
 		register_block_type_from_metadata(
-			$this->plugin->dir(),
-			[
-				'render_callback' => [ $this, 'render_callback' ],
-			]
+				$this->plugin->dir(),
+				[
+						'render_callback' => [ $this, 'render_callback' ],
+				]
 		);
 	}
 
 	/**
 	 * Renders the block.
 	 *
-	 * @param array    $attributes The attributes for the block.
-	 * @param string   $content    The block content, if any.
-	 * @param WP_Block $block      The instance of this block.
+	 * @param array $attributes The attributes for the block.
+	 * @param string $content The block content, if any.
+	 * @param WP_Block $block The instance of this block.
+	 *
 	 * @return string The markup of the block.
 	 */
 	public function render_callback( $attributes, $content, $block ) {
-		$post_types = get_post_types(  [ 'public' => true ] );
+		$post_types = get_post_types( [ 'public' => true ] );
 		$class_name = $attributes['className'];
 		ob_start();
 
 		?>
-        <div class="<?php echo $class_name; ?>">
+		<div class="<?php echo $class_name; ?>">
 			<h2>Post Counts</h2>
 			<ul>
-			<?php
-			foreach ( $post_types as $post_type_slug ) :
-                $post_type_object = get_post_type_object( $post_type_slug  );
-                $post_count = count(
-                    get_posts(
-						[
-							'post_type' => $post_type_slug,
-							'posts_per_page' => -1,
-						]
-					)
-                );
+				<?php
+				foreach ( $post_types as $post_type_slug ) :
+					$post_type_object = get_post_type_object( $post_type_slug );
+					$post_count = count(
+							get_posts(
+									[
+											'post_type'      => $post_type_slug,
+											'posts_per_page' => - 1,
+									]
+							)
+					);
 
-				?>
-				<li><?php echo 'There are ' . $post_count . ' ' .
-					  $post_type_object->labels->name . '.'; ?></li>
-			<?php endforeach;	?>
-			</ul><p><?php echo 'The current post ID is ' . $_GET['post_id'] . '.'; ?></p>
+					?>
+					<li><?php echo 'There are ' . $post_count . ' ' . $post_type_object->labels->name . '.'; ?></li>
+				<?php endforeach; ?>
+			</ul>
+			<p><?php echo 'The current post ID is ' . $_GET['post_id'] . '.'; ?></p>
 
 			<?php
-			$query = new WP_Query(  array(
-				'post_type' => ['post', 'page'],
-				'post_status' => 'any',
-				'date_query' => array(
-					array(
-						'hour'      => 9,
-						'compare'   => '>=',
-					),
-					array(
-						'hour' => 17,
-						'compare'=> '<=',
-					),
-				),
-                'tag'  => 'foo',
-                'category_name'  => 'baz',
-				  'post__not_in' => [ get_the_ID() ],
-				  'meta_value' => 'Accepted',
-			));
+			$query = new WP_Query(
+					[
+							'post_type'     => [ 'post', 'page' ],
+							'post_status'   => 'any',
+							'date_query'    => [
+									[
+											'hour'    => 9,
+											'compare' => '>=',
+									],
+									[
+											'hour'    => 17,
+											'compare' => '<=',
+									],
+							],
+							'tag'           => 'foo',
+							'category_name' => 'baz',
+							'post__not_in'  => [ get_the_ID() ],
+							'meta_value'    => 'Accepted',
+					]
+			);
 
 			if ( $query->found_posts ) :
 				?>
-				 <h2>Any 5 posts with the tag of foo and the category of baz</h2>
-                <ul>
-                <?php
+				<h2>Any 5 posts with the tag of foo and the category of baz</h2>
+				<ul>
+					<?php foreach ( array_slice( $query->posts, 0, 5 ) as $post ) : ?>
+						<li><?php echo $post->post_title ?></li>
+					<?php endforeach; ?>
+				</ul>
+			<?php endif; ?>
 
-                 foreach ( array_slice( $query->posts, 0, 5 ) as $post ) :
-                    ?><li><?php echo $post->post_title ?></li><?php
-				endforeach;
-			endif;
-		 	?>
-			</ul>
 		</div>
 		<?php
 
